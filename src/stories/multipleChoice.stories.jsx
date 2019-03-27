322ae6c3aa15aac8f8707519f185f761bbbf8715
Monte Nichols <montenichols@precocityllc.com>
2019-03-27 16:12:52 -0500

    added story-router to the route story, this was the cause of many of the bugs seen today.

--- src/stories/multipleChoice.stories.jsx
+++ src/stories/multipleChoice.stories.jsx
@@ -4,6 +4,4 @@ import { storiesOf } from '@storybook/react';
 
 import MultipleChoice from '../components/MultipleChoice';
 
-const noop = () => {};
-
 storiesOf('My MultipleChoice', module).add('default', () => <MultipleChoice match={{ params: { id: 2 } }} />);
--- src/stories/questionList.stories.jsx
+++ src/stories/questionList.stories.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 import QuestionList from '../components/QuestionList';
 
 const noop = () => {};
 
-storiesOf('My QuestionList', module).add('default', () => <QuestionList />);
+storiesOf('My QuestionList', module)
+    .addDecorator(StoryRouter())
+    .add('default', () => <QuestionList />);
