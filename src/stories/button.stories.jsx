07abf2b14e78e8b6f3d6acc9d6d8d18504b619e7
Monte Nichols <montenichols@precocityllc.com>
2019-03-26 21:54:06 -0500

    fixed a bug on the button story

--- src/stories/button.stories.jsx
+++ src/stories/button.stories.jsx
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 
 import Button from '../components/Button';
 
+const noop = () => {};
+
 storiesOf('My Button', module)
-    .add('default', () => <Button foo={''} />)
-    .add('disabled', () => <Button disabled={true} />);
+    .add('default', () => <Button foo={''} clickFn={noop} />)
+    .add('disabled', () => <Button disabled={true} clickFn={noop} />);
